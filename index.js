3ea31ebd41e4707d41c8543cbe302492597e23a6
mahfuz47 <mahfuzshikder560@gmail.com>
2022-05-24 19:03:38 +0600

    initial setup done

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Gadget manufacturer sites server is running");
+});
+
+app.listen(port, () => {
+  console.log("server is running on port", port);
+});
