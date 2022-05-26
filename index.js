3915d8d190471e87888bfd2567941a09f8603e05
mahfuz47 <mahfuzshikder560@gmail.com>
2022-05-26 23:00:16 +0600

    get admin auth

--- index.js
+++ index.js
@@ -91,6 +91,13 @@ async function run() {
       }
     });
 
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
     app.delete("/users/:email", verifyJWT, async (req, res) => {
       const email = req.params.email;
       const filter = { email: email };
