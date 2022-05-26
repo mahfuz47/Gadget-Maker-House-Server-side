1db72cfaf6eff753bc5734c70e46743613c87fdd
mahfuz47 <mahfuzshikder560@gmail.com>
2022-05-26 22:38:09 +0600

    user make admin and handle

--- index.js
+++ index.js
@@ -72,6 +72,25 @@ async function run() {
       console.log(token);
       res.send({ result, accessToken: token });
     });
+
+    app.put("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({
+        email: requester,
+      });
+      if (requesterAccount.role === "admin") {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "forbidden" });
+      }
+    });
+
     //
     //
     //
