ca9f338867dc88eb286368664bb84b17910f43c7
Mahfuzur Rahman <mahfuzshikder560@gmail.com>
2023-01-18 20:14:03 +0600

    updating

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000;
 const app = express();
 const jwt = require("jsonwebtoken");
-const query = require("express/lib/middleware/query");
 app.use(cors());
 app.use(express.json());
 
